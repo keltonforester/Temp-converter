b7a020b9fcc8bfac3a554745d751f8ce9a629bc6
Kelton Forester <keltonforester@Keltons-MacBook-Pro.local>
2019-12-16 13:52:05 -0700

    First commit

---
+++ temp converter2.py
@@ -0,0 +1,8 @@
+def main() :
+    print ("This program calculates Celcius to Fahrenheit for the people that")
+    print ("that are like me and can't understand Celcius.")
+main()
+
+celsius = eval(input("What is the Celsius temperature? "))
+Fahrenheit = 9/5 * celsius + 32
+print ("The temperature is" , Fahrenheit, " degrees Fahrenheit")
